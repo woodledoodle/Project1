e16d559dd36c91184bdc7bbee0248891b7ed6a8f
Anthony Bui <anthonybui@Anthonys-MBP.cfl.rr.com>
2019-09-17 16:36:46 -0400

    sequence of colors

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
+
 document.querySelector('#square1').addEventListener('click', function() {
-    document.getElementsByClassName('square11').style.square11
+  document.getElementsByClassName('square11')[0].style.backgroundColor = 'lightcoral'
+   
+
+   
+    
 });
 
 document.querySelector('#square2').addEventListener('click', function() {
@@ -14,3 +20,19 @@ document.querySelector('#square4').addEventListener('click', function() {
     console.log("youve been clicked")
 });
 //testtest
+
+function sequence(){
+    document.getElementsByClassName('square11')[0].style.backgroundColor = 'lightcoral'
+    setTimeout(function(){
+        document.getElementsByClassName('square11')[0].style.backgroundColor = 'red'
+    },2000)
+
+    document.getElementsByClassName('square22')[0].style.backgroundColor = 'lightblue'
+    setTimeout(function(){
+        document.getElementsByClassName('square22')[0].style.backgroundColor = 'blue'
+    },2000)
+
+//     document.getElementsByClassName('square33')[0].style.backgroundColor = 'lightcoral'
+
+//     document.getElementsByClassName('square44')[0].style.backgroundColor = 'lightcoral'
+}
