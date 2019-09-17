3b34033eac387a7ca153b8b0c7ec68fe7b50c4c6
Anthony Bui <anthonybui@Anthonys-MacBook-Pro.local>
2019-09-17 15:22:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('#square1').addEventListener('click', function() {
+    document.getElementsByClassName('square11').style.square11
+});
+
+document.querySelector('#square2').addEventListener('click', function() {
+    console.log("youve been clicked")
+});
+
+document.querySelector('#square3').addEventListener('click', function() {
+    console.log("youve been clicked")
+});
+
+document.querySelector('#square4').addEventListener('click', function() {
+    console.log("youve been clicked")
+});
+//testtest
